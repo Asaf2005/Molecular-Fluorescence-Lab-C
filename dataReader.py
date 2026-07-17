7665c911bdf1c1908ca157d7dd448569e6987c79
Asaf2005 <asafisivan@gmail.com>
2026-07-17 17:39:26 +0300

    constantLengthAnalysis

--- dataReader.py
+++ dataReader.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import os
 
-sheets_names = []
-df = pd.read_excel("data/0_1mM.ods", engine="odf")
 
-print(df.head())
 
+files = os.listdir("data")
+sheets_names = ['Fluoresceine','Rhodamine B','Rhodamine 6G']
+
+
+def read_data(file_path, sheet_name):
+    df = pd.read_excel(file_path, sheet_name=sheet_name, engine="odf")
+    return df
+
+print(os.path.join("data", files[0]))
+print(read_data(os.path.join("data", files[0]), "Rhodamine B).head())
